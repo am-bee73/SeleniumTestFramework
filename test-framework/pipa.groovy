01058935b8635933e24e8493e9d371f851b627c5
am-bee73 <53939796+am-bee73@users.noreply.github.com>
2022-11-07 19:19:37 +0200

    Create pipa.groovy

---
+++ test-framework/pipa.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        
+        stage("jora bora") {
+            steps {
+                script {
+                    sh "pwd"
+                }
+            }
+        }
+        
+     
+    }
+}
