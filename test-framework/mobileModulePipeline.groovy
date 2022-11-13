9c37140e98f03fe1c126375bf70f872a90139e2b
aiordan <ana-maria.iordan@endava.com>
2022-11-13 17:27:54 +0200

    add mobileModulePipeline.groovy

---
+++ test-framework/mobileModulePipeline.groovy
@@ -0,0 +1,57 @@
+#!Groovy
+
+pipeline {
+    agent any
+
+    tools {
+        maven "Apache Maven 3.6.3"
+    }
+
+    stages {
+
+        stage("mvn test") {
+            steps {
+                script {
+                    sh """
+                        cd test-framework/appium-mobile-testing/
+                        mvn -Dmaven.test.failure.ignore=true clean test 
+                        """
+                }
+            }
+        }
+
+        stage('Allure report') {
+            steps {
+                script {
+                    allure([
+                            includeProperties: false,
+                            jdk              : '',
+                            properties       : [],
+                            reportBuildPolicy: 'ALWAYS',
+                            results          : [[path: 'test-framework/appium-mobile-testing/target/allure-results']]
+                    ])
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'One way or another, I have finished !'
+        }
+        success {
+            script {
+                echo "SUCCESSFUL"
+            }
+        }
+        unstable {
+            echo "UNSTABLE"
+        }
+        failure {
+            script {
+                echo "FAILED"
+            }
+        }
+    }
+
+}
